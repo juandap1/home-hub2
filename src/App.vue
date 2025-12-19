5363b5ded0b3d77ab4be7c6fe4c1e589f96758dd
Matthew Branning <48932625+juandap1@users.noreply.github.com>
2025-12-18 20:16:23 -0500

    refreshData

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,37 @@ export default defineComponent({
       store,
     }
   },
+  data() {
+    return {
+      calendarInterval: null,
+      newsInterval: null,
+      weatherInterval: null,
+    }
+  },
   mounted() {
-    this.store.getWeather()
     this.store.getPictures()
-    this.store.getNews()
-    this.store.getCalendar()
+    const refreshData = () => {
+      this.store.getWeather()
+    }
+
+    // Initial fetch
+    refreshData()
+
+    // Refresh every minute
+    this.calendarInterval = setInterval(this.store.getCalendar, 60 * 1000)
+    this.newsInterval = setInterval(this.store.getNews, 30 * 60 * 1000) // Every 30 minutes
+    this.weatherInterval = setInterval(refreshData, 15 * 60 * 1000) // Every 15 minutes
+  },
+  unmounted() {
+    if (this.calendarInterval) {
+      clearInterval(this.calendarInterval)
+    }
+    if (this.newsInterval) {
+      clearInterval(this.newsInterval)
+    }
+    if (this.weatherInterval) {
+      clearInterval(this.weatherInterval)
+    }
   },
 })
 </script>
